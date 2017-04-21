143d288b6bcaa950c4bb4a6df7d1a3cb4ee7313a
Andy Hook <me@andrewhook.uk>
2017-04-21 23:16:53 +0100

    initial commit

---
+++ src/Core/Application.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ascend\Core;
+
+class Application
+{
+    /**
+     * @return [type]
+     */
+    public function run()
+    {
+        echo "The application is running...";
+    }
+}
